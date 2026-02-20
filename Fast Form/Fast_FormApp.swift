5e1c8ae2094adce452267165e23b02b0e68f31e3
aliberatdervisoglu <aliberatdervisoglu@gmail.com>
2026-02-20 21:24:33 +0300

    Initial Commit

---
+++ Fast Form/Fast_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fast_FormApp.swift
+//  Fast Form
+//
+//  Created by Ali Berat Dervişoğlu on 20.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Fast_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
