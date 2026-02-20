add07f222771760668ba8c16d5bf9e512ba35d4f
aliberatdervisoglu <aliberatdervisoglu@gmail.com>
2026-02-21 01:13:19 +0300

    integrate Firebase SDK

--- Fast Form/Others/Fast_FormApp.swift
+++ Fast Form/Others/Fast_FormApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct Fast_FormApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
