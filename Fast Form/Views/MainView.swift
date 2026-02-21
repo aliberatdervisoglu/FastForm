ee701e89febb9de3be6c7b5e3511ab9dd8ee0670
aliberatdervisoglu <aliberatdervisoglu@gmail.com>
2026-02-22 02:12:37 +0300

    signin control on mainview

--- Fast Form/Views/MainView.swift
+++ Fast Form/Views/MainView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct MainView: View {
+    @StateObject var viewModel = MainViewViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if viewModel.isSignedIn, !viewModel.currentUserID.isEmpty{
+            FormBuilderView()
+        } else {
+            LoginView()
+        }
     }
 }
 
