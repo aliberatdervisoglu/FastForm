21b4558ad310fdeead8f8e74975deaecbf7865cb
aliberatdervisoglu <aliberatdervisoglu@gmail.com>
2026-02-21 22:20:33 +0300

    log in view created

--- Fast Form/Views/LoginView.swift
+++ Fast Form/Views/LoginView.swift
@@ -7,12 +7,53 @@
 
 import SwiftUI
 
-struct LogInView: View {
+struct LoginView: View {
+    
+    @State private var email: String = "kmkm"
+    @State private var password: String = "knkm"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack(spacing: 25){
+                Image("headerImage")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.bottom, -40)
+                    
+                Form{
+                    TextField("Email Address..", text: $email)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .listRowBackground(Color.white)
+                    SecureField("Password...", text: $password)
+                        .listRowBackground(Color.white)
+                }
+                .scrollContentBackground(.hidden)
+                .frame(height: 150)
+                .scrollDisabled(true)
+                .foregroundColor(.black)
+                
+                Text("  ") // Error part will be update
+                BigButtonView(title: "Log In"){
+                    print("edildi")
+                }
+                Spacer()
+                VStack(spacing: 5){
+                    Text("Are you new here?")
+                        .foregroundColor(.secondary)
+                    NavigationLink("Sign Up",destination: RegisterView())
+                        .foregroundStyle(LinearGradient.brandGradient)
+                        .font(Font.system(size: 20, weight: .bold, design: .default))
+                }
+                .padding(.bottom,40)
+
+            }
+            .background(Color.white)
+        }
     }
 }
 
 #Preview {
-    LogInView()
+    LoginView()
 }
+
