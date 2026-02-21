53717c89588740754df876a0472b67259ff0e972
aliberatdervisoglu <aliberatdervisoglu@gmail.com>
2026-02-22 00:36:18 +0300

    translation extension for user struct

---
+++ Fast Form/Others/userEncodableExtension.swift
@@ -0,0 +1,24 @@
+//
+//  userEncodableExtension.swift
+//  Fast Form
+//
+//  Created by Ali Berat Dervişoğlu on 22.02.2026.
+//
+
+import Foundation
+
+extension Encodable{
+    func asDictionary() -> [String: Any]{
+        guard let data = try? JSONEncoder().encode(self) else{ // data:binary version of json(key value pair)
+            return [:]
+        }
+        do{
+            let json = try JSONSerialization.jsonObject(with: data) as? [String: Any] //as?: try, if there is an error return nil
+            return json ?? [:]
+
+        }catch{
+            return [:]
+        }
+    }
+    // guard and after try structure is same with do-catch and try
+}
