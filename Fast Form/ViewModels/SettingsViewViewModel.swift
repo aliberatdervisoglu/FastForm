40ea62317658d4906fa08a28a045781965cf17b1
aliberatdervisoglu <aliberatdervisoglu@gmail.com>
2026-03-16 00:28:38 +0300

    settings view structure and log out, delete accounts are works

--- Fast Form/ViewModels/SettingsViewViewModel.swift
+++ Fast Form/ViewModels/SettingsViewViewModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+import Combine
+import FirebaseAuth
+import FirebaseFirestore
+
+class SettingsViewViewModel: ObservableObject {
+
+    @Published var errormeessage: String? = nil
+    @Published var isLoading = false
+    @Published var showReauthAlert = false
+
+    init() {
+        
+    }
+
+    func logOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            self.errormeessage = "Log Out failed: \(error.localizedDescription)"
+        }
+    }
+    
+    
+    func deleteAccount(onSuccess: @escaping () -> Void) {
+        guard let currentUser = Auth.auth().currentUser else {return}
+        let userID = currentUser.uid
+        let db = Firestore.firestore()
+        
+        self.isLoading = true // for just one touch to butto
+        
+        db.collection("users").document(userID).delete() { [weak self] error in
+            if let error = error {
+                self?.handleError(error)
+                return
+            }
+            
+            currentUser.delete(){ [weak self] error in
+                self?.isLoading = false
+                
+                if let error = error as NSError? { // type cast for take an ERROR CODE
+                    if error.code == AuthErrorCode.requiresRecentLogin.rawValue { // code: 17014
+                        self?.showReauthAlert = true
+                    } else {
+                        self?.handleError(error)
+                    }
+                } else {
+                    onSuccess()
+                }
+            }
+        }
+    }
+    private func handleError(_ error: Error) {
+        self.isLoading = false
+        self.errormeessage = error.localizedDescription
+    }
+}
--- Fast Form/Views/SettingsView.swift
+++ Fast Form/Views/SettingsView.swift
@@ -9,8 +9,108 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @StateObject var viewModel = SettingsViewViewModel()
+    
+    @State var showDeleteConfirmation: Bool = false
+    @State var logOutConfirmation: Bool = false
+    
+
     var body: some View {
-        Text("settings")
+        NavigationStack{
+            ZStack{
+                Color(uiColor: .systemGroupedBackground).ignoresSafeArea()
+                VStack(alignment: .center, spacing: 15) {
+                    Image(systemName: "gear.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundStyle(Color("BrandGradientEnd"))
+                        .frame(width: 125, height: 125)
+                        .padding()
+                        .background(Color(uiColor: .secondarySystemGroupedBackground))
+                        .clipShape(Circle())
+                        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+                        .padding(.bottom, 10)
+                    VStack(spacing: 20){
+                        setRawView(title: "Set Name", iconname: "person") {
+                            print("trial1")
+                        }
+                        
+                        Divider()
+                        setRawView(title: "Set Mail", iconname: "at") {
+                            print("trial1")
+                        }
+                        
+                        Divider()
+                        setRawView(title: "Set Password", iconname: "lock") {
+                            print("trial1")
+                        }
+                        
+                        Divider()
+                        setRawView(title: "Log Out", iconname: "rectangle.portrait.and.arrow.right", optionalColor: .blue) {
+                            print("trial2")
+                        }
+                        Divider()
+                        setRawView(title: "Delete Account", iconname: "person.crop.circle.badge.minus", optionalColor: .red) {
+                            showDeleteConfirmation = true
+                        }
+                    }
+                    .padding()
+                    .background(Color(uiColor: .secondarySystemGroupedBackground))
+                    .cornerRadius(20)
+                    .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 2)
+                    .padding(.horizontal)
+
+                }
+            }
+            .alert("You are logging out...", isPresented: $logOutConfirmation) {
+                Button("Log Out") {
+                    viewModel.logOut()
+                }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Are you sure you want to log out?")
+            }
+            .alert("Are you absolutely sure", isPresented: $showDeleteConfirmation) {
+                Button("Delete", role: .destructive) {
+                    viewModel.deleteAccount {
+                    }
+                }
+                Button("Cancel", role: .cancel){ }
+            } message: {
+                Text("This action cannot be undone. Are you sure you want to delete your account?")
+            }
+            .alert("Security Re-Authentication", isPresented: $viewModel.showReauthAlert) {
+                Button("Log Out & In Again") {
+                    viewModel.logOut()
+                }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("For your security, you must have logged in recently to delete your account. Please log out and log back in, then try again.")
+            }
+        }
+    }
+    @ViewBuilder
+    func setRawView(title: String ,iconname: String,optionalColor: Color? = nil, action: @escaping () -> Void) -> some View {
+        Button {
+            action()
+        } label: {
+            HStack{
+                ZStack {
+                    Circle()
+                        .frame(width: 42, height: 42)
+                        .foregroundStyle(Color("BrandGradientEnd").opacity(0.1))
+                    
+                    Image(systemName: iconname)
+                        .foregroundStyle(Color("BrandGradientEnd"))
+                        .font(.system(size: 24))
+                }
+                Text(title)
+                    .font(.title3)
+                    .foregroundColor(optionalColor ?? .primary)
+                    .fontWeight(.semibold)
+                Spacer()
+            }
+        }
     }
 }
 
