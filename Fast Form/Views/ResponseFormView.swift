319b98b87dd6439212e6e715e6ebf218424b8d21
aliberatdervisoglu <aliberatdervisoglu@gmail.com>
2026-03-21 20:45:34 +0300

    search to responce part revise

--- Fast Form/Views/ResponseFormView.swift
+++ Fast Form/Views/ResponseFormView.swift
@@ -14,19 +14,28 @@ struct ResponseFormView: View {
     var body: some View {
         NavigationStack{
             
-            Color(uiColor: .systemGroupedBackground).ignoresSafeArea()
-            List(viewModel.results) { form in
-                NavigationLink(destination: ResponseChoosenFormView(form: form)) {
-                    VStack(alignment: .leading) {
-                        Text(form.title)
-                            .font(.headline)
-                        Text(form.explanation)
-                            .font(.subheadline)
-                            .foregroundStyle(.secondary)
+            ZStack{
+                Color(uiColor: .systemGroupedBackground).ignoresSafeArea()
+                if viewModel.results.isEmpty && viewModel.searchText.isEmpty {
+                    ContentUnavailableView("Search for a Form",
+                                           systemImage: "magnifyingglass",
+                                           description: Text("Type a title to find and fill out a form."))
+                } else {
+                    List(viewModel.results) { form in
+                        NavigationLink(destination: ResponseChoosenFormView(form: form)) {
+                            VStack(alignment: .leading) {
+                                Text(form.title)
+                                    .font(.headline)
+                                Text(form.explanation)
+                                    .font(.subheadline)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
                     }
+                    .scrollContentBackground(.hidden)
+                    
                 }
             }
-            .scrollContentBackground(.hidden)
             .background(Color(uiColor: .systemGroupedBackground))
             .navigationTitle("Find Form")
             .searchable(text: $viewModel.searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Enter a Form Title...")                .onChange(of: viewModel.searchText) {
