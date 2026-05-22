dfc46eb0be0b22df97f92b23cfac01ea4e9dd94b
aliberatdervisoglu <aliberatdervisoglu@gmail.com>
2026-05-22 21:42:18 +0300

    small fix

--- Fast Form/Features/Forms/FormListView.swift
+++ Fast Form/Features/Forms/FormListView.swift
@@ -15,7 +15,7 @@ struct FormListView: View {
     
     
     init(userId: String){
-        self._viewModel = StateObject(wrappedValue: FormListViewViewModel(userId: userId))
+        self._viewModel = State(wrappedValue: FormListViewViewModel(userId: userId))
     }
     var body: some View {
         NavigationStack{
